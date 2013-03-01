a0cb82700c01e0b8b2065a31b032e31e272d482b
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-02-28 21:40:54 -0500

    JsHint fixes

--- scraper.js
+++ scraper.js
@@ -1,16 +1,20 @@
-var request = require("request");
+/*jshint node:true*/
+"use strict";
+
+var request = require("request"),
+    fs = require("fs");
 
 var rePages = /a href=\"http:\/\/www.althingi.is\/lagas\/141a\/(.*)?\.html\"/g,
     reLinks = /a href=\"([^\/]*)\.html?\">/g,
     reTitle = /<title>.*\/(.*)?<\/title>/;
 
-var pages = {},linksno=0;
+var pages = {};
 
 request("http://www.althingi.is/lagas/nuna/lagas.nr.html",function(error,response,body) {
-  var page,cnt=0;
+  var page;
   while (page = rePages.exec(body)) pages[page[1]]={links:{}};
   openPages();
-})
+});
 
 function openPages() {
   var keys = Object.keys(pages),
@@ -22,15 +26,14 @@ function openPages() {
       title = reTitle.exec(body);
       if (title) pages[page].title = title[1];
       pages[page].size = body.length;
-      while (link = reLinks.exec(body)) {linksno++;pages[page].links[link[1]]=1;}
-      console.log(i)
+      while (link = reLinks.exec(body)) pages[page].links[link[1]]=1;
+      console.log(i);
       if (!(--i)) done();
-    })
-  })
-
-};
+    });
+  });
+}
 
 function done() {
-  require("fs").writeFileSync("log.json",JSON.stringify(pages));
-  console.log("done")
+  fs.writeFileSync("log.json",JSON.stringify(pages));
+  console.log("done");
 }
